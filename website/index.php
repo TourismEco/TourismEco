668c717748361d95a949ecba79b7b8b275591d43
hugoglvs <104032491+hugoglvs@users.noreply.github.com>
2023-09-27 12:36:58 +0200

    Creating website directory and index.php

---
+++ website/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="FR-fr">
+    <head>
+        <title>EcoTourisme</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet"  href="..assets/css/style.css">
+    </head>
+    
+    <body>
+        <?php echo "<p> SALUT</p>" ?>
+    </body>
+</html>
