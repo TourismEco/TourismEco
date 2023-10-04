93b06a73d5033278c9c3def410585f2dadb729ed
hugoglvs <hugogoncalves@MacBook-Pro-de-Hugo.local>
2023-10-04 23:15:06 +0200

    Created "pre-projet" directory and added UML use-case diagram

--- website/getDB.php
+++ website/getDB.php
@@ -9,4 +9,15 @@
         return new mysqli($servername, $username, $password, $dbname);
     }
 
+    /* Pour utiliser la database:
+    Aller dans le fichier ou l'on souhaite l'utiliser
+    require "path/to/getDB.php";
+    $conn = getDb();
+    $mysql = "METTRE VOTRE REQUETE";
+    $conn->query($mysql);
+
+    n'oubliez pas de changer "path/to/ par le chemin adapte
+    variable $mysql nomme comme ca car j'aurais voulu l'appeler
+    query mais c'est deja le nom de la fonction qui fait la requete
+    */
 ?>
