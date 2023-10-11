5b89eec48fcef536dd1b741fb7c7f80925417336
Cassandra Sénécaille <senecaille.cassandra34@gmail.com>
2023-10-11 11:34:26 +0200

    Page Pays V1
    Version 1 de la page Pays, manque que les scores et a arranger deux trois trucs

---
+++ website/Pays_V1/courbe/barres/en_barres.php
@@ -0,0 +1,138 @@
+<?php
+
+// Votre connexion MySQLi
+$conn = new mysqli('127.0.0.1', 'root', 'root', 'Ecotourisme', 3306);
+
+if ($conn->connect_error) {
+    die("Erreur de connexion à la base de données : " . $conn->connect_error);
+}
+
+$query = "
+SELECT pays.nom, ROUND(SUM(co2)) as total
+FROM ecologie, pays
+WHERE pays.id = ecologie.id_pays
+GROUP BY id_pays  
+ORDER BY `total` DESC
+LIMIT 10;
+";
+
+$result = $conn->query($query);
+
+$report_data = array();
+while ($rs = $result->fetch_assoc()) {
+    $report_data[] = '
+    {country:' . '"' . $rs['nom'] . '"' . ',
+    ' . 'value:' . $rs['total'] . '}';
+}
+
+// Concaténer les données
+$report_data = implode(",", $report_data);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ECOTOURSIME</title>
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <!-- amcharts devbanban.com -->
+    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+    <!-- Définir la taille du graphique -->
+    <style>
+        #chartdiv {
+            width: 100%;
+            height: 500px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10"> <br>
+           
+            <div id="chartdiv"></div>
+            <script>
+                // DEBUT SCRIPT 
+                var root = am5.Root.new("chartdiv");
+
+                // Définir les thèmes
+                root.setThemes([
+                    am5themes_Animated.new(root)
+                ]);
+
+                // Créer le graphique
+                var chart = root.container.children.push(am5xy.XYChart.new(root, {
+                    panX: true,
+                    panY: true,
+                    wheelX: "panX",
+                    wheelY: "zoomX"
+                }));
+
+                // Ajouter le curseur
+                var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+                cursor.lineY.set("visible", false);
+
+                // Créer les axes
+                var xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
+                xRenderer.labels.template.setAll({
+                    rotation: -90,
+                    centerY: am5.p50,
+                    centerX: am5.p100,
+                    paddingRight: 15
+                });
+
+                var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+                    maxDeviation: 0.3,
+                    categoryField: "country",
+                    renderer: xRenderer,
+                    tooltip: am5.Tooltip.new(root, {})
+                }));
+
+                var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+                    maxDeviation: 0.3,
+                    renderer: am5xy.AxisRendererY.new(root, {})
+                }));
+
+                // Créer la série
+                var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+                    name: "Series 1",
+                    xAxis: xAxis,
+                    yAxis: yAxis,
+                    valueYField: "value",
+                    sequencedInterpolation: true,
+                    categoryXField: "country",
+                    tooltip: am5.Tooltip.new(root, {
+                        labelText:"{valueY}"
+                    })
+                }));
+
+                series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5 });
+                series.columns.template.adapters.add("fill", (fill, target) => {
+                    return chart.get("colors").getIndex(series.columns.indexOf(target));
+                });
+
+                series.columns.template.adapters.add("stroke", (stroke, target) => {
+                    return chart.get("colors").getIndex(series.columns.indexOf(target));
+                });
+
+                // Définir les données
+                var data = [<?= $report_data;?>];
+
+                xAxis.data.setAll(data);
+                series.data.setAll(data);
+
+                // Animer les éléments lors du chargement
+                series.appear(1000);
+                chart.appear(1000, 100);
+            </script>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ website/Pays_V1/pays.php
@@ -0,0 +1,90 @@
+<meta charset="utf-8">
+<html lang="fr">
+
+    <head>
+        <link rel="stylesheet" href="styles/styles.css" type="text/css" media="screen" />
+        <title>Pays</title>
+    </head>
+    <body >
+        <div class="topnav">
+            <img class="logoNavBar" src="images/logo.png" alt="logo">
+            <a class="active" href="#home">Accueil</a>
+            <a classe="no-active" href="#news">Comparateur</a>
+            <a classe="no-active" href="#contact">Analyses</a>
+
+            <input class ="search" type="search" id="site-search" name="q" />
+
+            <button class="favorite styled" type="button">S'inscrire</button>
+            <button class="favorite styled2" type="button">Se connecter</button>
+
+        </div>
+
+        <div class="map">
+            <iframe src="map.html" width="100%" height="60%"></iframe>
+        </div>
+
+        <div class="info">
+            <iframe src="bandeau.php" width="100%" height="36%"></iframe>
+        </div>
+
+        <div class="score">
+            <p>EcoToursim Score
+                </br>La France a un score A ! C’est excellent !
+                </br>Le pays se place au 5ème rang mondial, et 1er à l’échelle de son continent.
+
+            </p>
+        </div>
+
+
+        
+
+        <div class="container">
+            <div class="texte">
+                <p>Evolution du PIB de la France dans le temps</p>
+                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.                
+                </p>
+            </div>
+            <div class="logoEcon">
+            <img src="images/economie.png" alt="LogoEconomie">
+            </div>
+            <div class="graphique">
+                <iframe src="courbe/courbe/pib_annee.php" width="650" height="530"></iframe>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="logoEcolo">
+                <img src="images/ecologie.png" alt="LogoEconomie">
+            </div>
+            <div class="graphique">
+                <iframe src="courbe/courbe/co2_annee.php" width="650" height="530"></iframe>
+            </div>
+            <div class="texte">
+                <p>Evolution du Co2 en France dans le temps</p>
+                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.                
+                </p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="texte">
+                <p>Evolution des dépenses et des recettes de la France dans le temps</p>
+                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.                
+                </p>
+            </div>
+            <div class="logoEcon">
+                <img src="images/economie.png" alt="LogoEconomie">
+            </div>
+            <div class="graphique">
+                <!-- Insérez votre graphique ici -->
+                <iframe src="courbe/double_courbe/depenses_recettes.php" width="650" height="530"></iframe>
+            </div>
+        </div>
+       
+
+            
+    </body>
+</html>
