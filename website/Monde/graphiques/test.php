63e8b8af1c9565ccf799b3eddefd8b10c7a7b0b8
Line Bransolle <bransolle.line@gmail.com>
2023-11-28 16:40:59 +0100

    page monde

---
+++ website/Monde/graphiques/test.php
@@ -0,0 +1,142 @@
+<DOCTYPE html>
+  <head>
+    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/hierarchy.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+    <script type="text/javascript" src="test.js"></script>
+  </head>
+<?php
+
+// Votre connexion MySQLi
+$conn = new mysqli('127.0.0.1', 'root', '', 'ecotourisme', 3306);
+
+if ($conn->connect_error) {
+    die("Erreur de connexion à la base de données : " . $conn->connect_error);
+}
+
+
+$query = "
+SELECT continents.nom as continent, pays.nom AS pays, SUM(tourisme.arriveesTotal) as arrivees_par_continent 
+FROM tourisme, continents, pays 
+WHERE tourisme.id_pays = pays.id AND pays.id_continent = continents.id 
+GROUP BY continents.nom;
+";
+
+$result = $conn->query($query);
+
+$chart_data = array();
+$report_data = array();
+
+while ($rs = $result->fetch_assoc()) {
+  $continent = $rs['continent'];
+  $arrivees = intval($rs['arrivees_par_continent']);
+  $pays = $rs['pays'];
+
+  if (!isset($chart_data[$continent])) {
+      $chart_data[$continent] = array(
+          "value" => 0,
+          "children" => array(),
+      );
+  }
+  $chart_data[$continent]["children"][] = array(
+    "name" => $pays,
+    "value" => $arrivees,
+);
+
+$chart_data[$continent]["value"] += $arrivees;
+}
+
+//tableau simple 
+$final_data = array();
+foreach ($chart_data as $continent => $data) {
+    $final_data[] = array(
+        "name" => $continent,
+        "value" => $data["value"],
+        "children" => $data["children"],
+    );
+}
+
+while ($rs = $result->fetch_assoc()) {
+    $report_data[] = array(
+      "value" => $rs['arrivees_par_continent'],
+      "continent" => $rs['continent']
+    );
+
+}
+
+// Fermez la connexion à la base de données
+$conn->close();
+?>
+
+<!-- Styles -->
+<style>
+#chartdiv {
+  width: 100%;
+  height: 500px;
+}
+</style>
+
+
+<!-- Chart code -->
+<script>
+am5.ready(function() {
+
+// Create root element
+// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+var root = am5.Root.new("chartdiv");
+
+// Set themes
+// https://www.amcharts.com/docs/v5/concepts/themes/
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
+
+// Create wrapper container
+var container = root.container.children.push(
+  am5.Container.new(root, {
+    width: am5.percent(100),
+    height: am5.percent(100),
+    layout: root.verticalLayout
+  })
+);
+
+// Create series
+// https://www.amcharts.com/docs/v5/charts/hierarchy/#Adding
+var series = container.children.push(
+  am5hierarchy.Treemap.new(root, {
+    singleBranchOnly: false,
+    downDepth: 1,
+    upDepth: -1,
+    initialDepth: 2,
+    valueField: "value",
+    categoryField: "name",
+    childDataField: "children",
+    nodePaddingOuter: 0,
+    nodePaddingInner: 0
+  })
+);
+
+series.rectangles.template.setAll({
+  strokeWidth: 2
+});
+
+// Generate and set data
+// https://www.amcharts.com/docs/v5/charts/hierarchy/#Setting_data
+var data = {
+    name: "Root",
+    children: <?php echo json_encode($final_data); ?>
+};
+
+series.data.setAll([data]);
+series.set("selectedDataItem", series.dataItems[0]);
+
+// Make stuff animate on load
+series.appear(1000, 100);
+
+}); // end am5.ready()
+</script>
+
+<!-- HTML -->
+<div id="chartdiv"></div>
+
+</html>
---
+++ website/Monde/monde.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang= "fr">
+
+<head>
+  <meta charset="UTF-8">
+  <link href="styles/style.css" rel="stylesheet" type="text/css" />
+  <title> Ecotourisme </title>
+
+  <?php
+    require 'functions/functions.php';
+    $bdd = getDB();
+  ?>
+
+</head>
+
+<body>
+    <div class = map> 
+    <!-- <iframe src="path-to-your-map-page.html" width="100%" height="500"></iframe> -->
+    </div>
+
+    <div class= bandeau> <!-- risque d'avoir un functions -->
+        <h1> Statistiques mondiales </h1>
+    </div>
+
+    <div class = section1>
+
+    </div>
+
+    <div class = section2>
+        <h2> Comparaison rapide des continents</h2>
+
+    </div>
+
+    <div class=footer>
+
+    </div>
+
+</body>
