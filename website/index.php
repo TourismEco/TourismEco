54e79c78329dc0e724c9ced149e5834d64080187
hugoglvs <104032491+hugoglvs@users.noreply.github.com>
2024-04-20 19:45:28 +0200

    Removed useless div.carrousel

--- website/index.php
+++ website/index.php
@@ -3,44 +3,43 @@
 <body>
   <div class="window">
   <div class="home" id="zones" hx-swap="outerHTML">
-      <div class ="carrousel">
         <?php
         $conn = getDB();
         carousel($conn);
         ?>
-        </div>
+
 
     <div class="sectionHome">
-      <div class="content" id="R">
+      <div class="content">
           <h2>Explorez le monde en équilibre</h2>
           <p>Bienvenue sur le site Écotourisme, votre destination en ligne pour explorer et comprendre les données économiques et écologiques de différentes régions du monde. Notre plateforme offre un éventail de fonctionnalités pour vous aider à planifier des voyages plus responsables, basés sur des informations pertinentes.</p>
       </div>
-      <img class="imgR" src="assets/icons/airBalloon.svg" alt="Logo 3">
+      <img src="assets/icons/airBalloon.svg" alt="Logo 3">
       <a hx-post="explorer.php" hx-push-url="true" hx-target="#zones" hx-select="#zones" hx-swap="outerHTML swap:0.5s" class="btnIndex">Explorez maintenant</a>
     </div>
 
     <div class="sectionHome">
-    <img  class="imgL" src="assets/icons/loupeIndex.svg" alt="Logo 3">
-    <a hx-get="analyse.php" hx-push-url="true" hx-target="#zones" hx-select="#zones" hx-swap="outerHTML swap:0.5s" class="btnIndex2">Page analyse</a>
-      <div class="content" id="L">
+    <img src="assets/icons/loupeIndex.svg" alt="Logo 3">
+    <a hx-get="analyse.php" hx-push-url="true" hx-target="#zones" hx-select="#zones" hx-swap="outerHTML swap:0.5s" class="btnIndex">Page analyse</a>
+      <div class="content">
         <h2>Comprendre en profondeur</h2>
         <p>Accédez à des rapports d'analyse statistique complets basés sur nos données, vous permettant de découvrir des tendances, des corrélations et des informations essentielles pour des décisions de voyage éclairées.</p>
       </div>
     </div>
 
     <div class="sectionHome">
-      <div class="content" id="R">
+      <div class="content">
         <h2>Voyagez responsable</h2>
         <p> Planifiez vos trajets en toute conscience environnementale. Notre calculateur d'empreinte carbone vous permet d'estimer l'impact de vos voyages en fonction des moyens de transport, vous aidant ainsi à prendre des décisions éclairées pour réduire votre empreinte carbone. </p>
       </div>
-        <img class="imgR" src="assets/icons/CO2.svg" alt="Logo 2">
+        <img src="assets/icons/CO2.svg" alt="Logo 2">
         <a hx-get="calculateur.php" hx-push-url="true" hx-target="#zones" hx-select="#zones" hx-swap="outerHTML swap:0.5s" class="btnIndex">Accédez au calculateur</a>
     </div>
 
     <div class="sectionHome">
-      <img class="imgL" src="assets/icons/balance.svg" alt="Logo 3">
-      <a hx-get="comparateur.php" hx-push-url="true" hx-target="#zones" hx-select="#zones" hx-swap="outerHTML swap:0.5s" class="btnIndex2">Accédez au comparateur</a>
-      <div class="content" id="L">
+      <img src="assets/icons/balance.svg" alt="Logo 3">
+      <a hx-get="comparateur.php" hx-push-url="true" hx-target="#zones" hx-select="#zones" hx-swap="outerHTML swap:0.5s" class="btnIndex">Accédez au comparateur</a>
+      <div class="content">
       <h2>Comparez deux pays</h2>
       <p>Découvrez les différences entre les impacts environnementaux de deux pays avec notre outil de comparaison. Visualisez les données à travers des graphiques clairs et explorez les indicateurs clés pour prendre des décisions éclairées.</p>
       <p>Comparer les performances environnementales des nations vous aide à choisir des partenaires commerciaux, des destinations de voyage et des politiques favorables à la durabilité.</p>
@@ -48,11 +47,11 @@
     </div>
 
       <div class="sectionHome">
-      <div class="content" id="R">
+      <div class="content">
         <h2>Comprendre l'impact des pays sur notre planète</h2>
         <p>Accédez aux différents scores que nous proposons pour comparer les impacts des pays sur notre planète. Explorez nos évaluations économiques, écologiques et de découvertes pour obtenir une vision holistique des contributions de chaque nation à notre environnement mondial. Notre score global offre une synthèse complète, vous permettant de prendre des décisions informées et de contribuer à un avenir plus durable. Découvrez comment chaque pays se positionne.</p>
       </div>
-      <img class="imgR"src="assets/icons/ScoreIndex.svg" alt="Logo 4">
+      <img src="assets/icons/ScoreIndex.svg" alt="Logo 4">
     </div>
 
 
