0258e3db76a4d32530259387111995dbfd750848
hugoglvs <104032491+hugoglvs@users.noreply.github.com>
2024-04-21 10:32:18 +0200

    Navbar responsive, félicitations Théodore pour le master

--- website/navbar.php
+++ website/navbar.php
@@ -1,29 +1,22 @@
+<?php require_once "head.php"; ?>
+
 <nav id="nav" class="navbar" hx-boost="true" hx-target="#zones" hx-select="#zones" hx-swap="outerHTML swap:0.5s show:window:top" hx-swap-oob="outerHTML">
-    <div>
-        <a href="index.php" aria-label="Accueil"><img class="img-nav" src="assets/icons/ecomini.png" alt="Logo TourismEco"></a>
+    <a href="index.php" aria-label="Accueil"><img class="img-nav" src="assets/icons/ecomini.png" alt="Logo TourismEco"></a>
+    <div class="nav-links">
         <a href="explorer.php" aria-label="MONDE">Explorer</a>
         <a href="pays.php" aria-label="Pays">Pays</a>
         <a href="comparateur.php" aria-label="Comparateur">Comparateur</a>
         <a href="continent.php" aria-label="Continent">Continent</a>
         <a href="analyse.php" aria-label="MONDE">Analyse</a>
         <a href="calculateur.php" aria-label="Calculateur">Calculateur</a>
-
-        <?php
-        if (isset($_SESSION['user'])) {
+    </div>
+    <div id="log-in">
+        <?php if (isset($_SESSION["user"])) {
             echo '<a href="profil.php" aria-label="Profil" id="n1">Profil</a>';
             echo '<a href="deconnexion.php" aria-label="Déconnexion" id="n2">Déconnexion</a>';
         } else {
             echo '<a href="inscription.php" aria-label="Inscription" id="n1">S\'inscrire</a>';
             echo '<a href="connexion.php" aria-label="Connexion" id="n2">Se connecter</a>';
-        }
-        ?>
-
-        <script>
-            $("a").on("click",function() {
-                $("#nav-bot").css("z-index","1")
-                $("#nav-bot").css("transform","translateX(-3000px)")
-            })
-        </script>
+        } ?>
     </div>
-
 </nav>
